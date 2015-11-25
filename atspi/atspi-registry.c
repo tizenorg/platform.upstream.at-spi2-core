09848d743072426f7cb73c81a8ce1e6535cec5c7
Bartlomiej Uliasz <b.uliasz@samsung.com>
2015-11-25 15:50:55 +0100

    atspi-registry.c: Updated description of event_types parameter in atspi_register_keystroke_listener() function
    Change-Id: I2670fd0b42c4c5e3eeeb590ea78a46097ef87807
    Signed-off-by: Bartlomiej Uliasz <b.uliasz@samsung.com>

--- atspi/atspi-registry.c
+++ atspi/atspi-registry.c
@@ -178,7 +178,9 @@ unregister_listener (gpointer data, GObject *object)
  *             #atspi_register_keystroke_listener once for each
  *             combination.
  * @event_types: an #AtspiKeyMaskType mask indicating which
- *             types of key events are requested (%ATSPI_KEY_PRESSED etc.).
+ *             types of key events are requested. 0 means all event types,
+ *             other values shoud be converted to mask, for example:
+ *             1 << ATSPI_KEY_RELEASED_EVENT.
  * @sync_type: an #AtspiKeyListenerSyncType parameter indicating
  *             the behavior of the notification/listener transaction.
  * @error: (allow-none): a pointer to a %NULL #GError pointer, or %NULL
