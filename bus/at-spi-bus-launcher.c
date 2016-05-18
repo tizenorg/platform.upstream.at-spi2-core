a2a13d5173e376805e148d7e1c0a882c102a93cc
shashank.p <shashank.p@samsung.com>
2016-05-18 14:58:19 +0530

    Kill Screen-reader via SIGTERM only if aul_terminate_pid fails(returns a negative value)
    Change-Id: I8e863f3cc0acc11306eeb6a7e2fce50c9e75b65a
    Signed-off-by: shashank.p <shashank.p@samsung.com>

--- bus/at-spi-bus-launcher.c
+++ bus/at-spi-bus-launcher.c
@@ -677,7 +677,7 @@ _terminate_screen_reader(A11yBusLauncher *bl)
      }
 
    LOGD("terminate process with pid %d", bl->pid);
-   if (!aul_terminate_pid(bl->pid))
+   if (aul_terminate_pid(bl->pid) > -1)
      {
         bl->pid = 0;
         return TRUE;
